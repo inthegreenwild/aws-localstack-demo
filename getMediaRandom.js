60bcbadd181e12b101005efa8b0fcbf5bba299a3
dylan cairns <dylan@dylans-Mac-mini.local>
2020-04-04 23:12:35 -0500

    init commit - WIP demo

---
+++ getMediaRandom.js
@@ -0,0 +1,74 @@
+var AWS = require("aws-sdk");
+
+var hostName = 'http://' + process.env.LOCALSTACK_HOSTNAME + ':4569'
+
+AWS.config.update({
+    region: "us-east-1",
+    endpoint: hostName,
+    accessKeyId: 'secret',
+    secretAccessKey: 'alsosecret',
+    maxRetries: 3
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+let handler = async (event, context) => {
+    const mediaType = event.queryStringParameters.type;
+    const mediaMap = {
+        'movie': 'Movie',
+        'tv': 'TV Show'
+    }
+    const mediaMapped = mediaMap[mediaType];
+
+    const scanParams = {
+        TableName: 'MovieDB',
+        FilterExpression: "#t = :t_val",
+        ExpressionAttributeNames: { '#t': 'type' },
+        ExpressionAttributeValues: { ':t_val': mediaMapped },
+        ProjectionExpression: "releaseYear, title"
+    };
+
+    let data = null;
+
+    try {
+        data = await docClient.scan(scanParams).promise();
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: e
+            }, null, 2)
+        }
+    }
+
+    let len = data.Items.length;
+    let ran = Math.floor(Math.random() * len);
+    let item = data.Items[ran];
+
+    var getParams = {
+        TableName: 'MovieDB',
+        Key: {
+            releaseYear: item.releaseYear,
+            title: item.title
+        }
+    }
+
+    try {
+        data = await docClient.get(getParams).promise();
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: e
+            }, null, 2)
+        }
+    }
+
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data, null, 2)
+    }
+}
+
+exports.handler = handler
---
+++ seed.js
@@ -0,0 +1,36 @@
+var AWS = require("aws-sdk");
+var fs = require('fs');
+
+var hostName = 'http://' + process.env.LOCALSTACK_HOSTNAME + ':4569'
+
+AWS.config.update({
+    endpoint: 'http://localhost:4569',
+    region: 'us-east-1',
+    accessKeyId: 'secret',
+    secretAccessKey: 'alsosecret'
+})
+
+var dynamodb = new AWS.DynamoDB.DocumentClient();
+
+
+var allMovies = JSON.parse(fs.readFileSync('seed.json', 'utf8'));
+allMovies.forEach(function (movie) {
+    var params = {
+        TableName: "MovieDB",
+        Item: {
+            releaseYear: movie.release_year,
+            title: movie.title,
+            description: movie.description,
+            type: movie.type,
+            duration: movie.duration
+        }
+    };
+
+    dynamodb.put(params, function (err, data) {
+        if (err) {
+            console.error("Unable to add movie", movie.title, ". Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("PutItem succeeded:", movie.title);
+        }
+    });
+});
